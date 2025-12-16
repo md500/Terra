f891024972ab068429021843c1f9bda414c8d7c9
Denis Golovin <golovin.denis@lenta.ru>
2025-12-17 01:14:45 +0500

    Backport: Clear NBT tag from Bukkit.createBlockData(extractId(data)) (because NBT is not supported here)

--- platforms/bukkit/common/src/main/java/com/dfsek/terra/bukkit/handles/BukkitWorldHandle.java
+++ platforms/bukkit/common/src/main/java/com/dfsek/terra/bukkit/handles/BukkitWorldHandle.java
@@ -1,24 +1,8 @@
-/*
- * This file is part of Terra.
- *
- * Terra is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Terra is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Terra.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 package com.dfsek.terra.bukkit.handles;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.block.data.BlockData;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,7 +12,6 @@ import com.dfsek.terra.api.entity.EntityType;
 import com.dfsek.terra.api.handle.WorldHandle;
 import com.dfsek.terra.bukkit.util.BukkitUtils;
 import com.dfsek.terra.bukkit.world.block.data.BukkitBlockState;
-
 
 public class BukkitWorldHandle implements WorldHandle {
     private static final Logger logger = LoggerFactory.getLogger(BukkitWorldHandle.class);
@@ -47,8 +30,7 @@ public class BukkitWorldHandle implements WorldHandle {
                 ". You are advised to perform this rename in your config backs as this translation will be removed in the next major " +
                 "version of Terra.");
         }
-        org.bukkit.block.data.BlockData bukkitData = Bukkit.createBlockData(
-            data); // somehow bukkit managed to make this not thread safe! :)
+        BlockData bukkitData = Bukkit.createBlockData(extractId(data));
         return BukkitBlockState.newInstance(bukkitData);
     }
 
@@ -59,6 +41,13 @@ public class BukkitWorldHandle implements WorldHandle {
 
     @Override
     public @NotNull EntityType getEntity(@NotNull String id) {
-        return BukkitUtils.getEntityType(id);
+        return BukkitUtils.getEntityType(extractId(id));
+    }
+
+    private static @NotNull String extractId(@NotNull String input) {
+        int brace = input.indexOf('{');
+        return brace == -1
+               ? input
+               : input.substring(0, brace).trim();
     }
 }
