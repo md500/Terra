2ad05cbbb6c96a6147a5e7722f1dd99cf10ba616
dfsek <dfsek@dfsek.com>
2025-12-29 23:21:31 -0700

    back ShortcutHolder with generic TypeLoader

--- common/implementation/base/src/main/java/com/dfsek/terra/registry/ShortcutHolder.java
+++ common/implementation/base/src/main/java/com/dfsek/terra/registry/ShortcutHolder.java
@@ -10,15 +10,14 @@ import java.lang.reflect.AnnotatedType;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.dfsek.terra.api.registry.Registry;
 import com.dfsek.terra.api.tectonic.ShortcutLoader;
 
 
 public class ShortcutHolder<T> implements TypeLoader<T> {
     private final Map<String, ShortcutLoader<T>> shortcuts = new HashMap<>();
-    private final Registry<T> back;
+    private final TypeLoader<T> back;
 
-    public ShortcutHolder(Registry<T> back) {
+    public ShortcutHolder(TypeLoader<T> back) {
         this.back = back;
     }
 
