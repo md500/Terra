b6d42fdcc1a1afb7b6e0c5cd6b71ddef400e82b9
dfsek <dfsek@dfsek.com>
2026-01-02 22:22:14 -0700

    rename biome-query-api to api-biome-query

--- common/addons/biome-provider-extrusion/build.gradle.kts
+++ common/addons/biome-provider-extrusion/build.gradle.kts
@@ -2,5 +2,5 @@ version = version("1.0.0")
 
 dependencies {
     compileOnlyApi(project(":common:addons:addon-loader-manifest"))
-    compileOnlyApi(project(":common:addons:biome-query-api"))
+    compileOnlyApi(project(":common:addons:api-biome-query"))
 }
